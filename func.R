b254f8e034b4f3857329c3ba054bd0a75dfc569d
Akiyama Hiroki <hiroki.akiyama.tokyo@gmail.com>
2020-03-14 14:42:23 +0900

    almost done! next: lightgbm

--- func.R
+++ func.R
@@ -180,18 +180,91 @@ my_test_confirm_plot <- function(pred_model) {
 
 
 
+
 # for classification
+
+# roc curve and auc -------------------------------------------------------
+
+roc_curve_and_auc <- function(res_models) {
+  tmp_auc <-
+    res_solo_models %>%
+    map(roc_auc, !!target_var, !!pred_positive) %>%
+    map2(.x = .,
+         .y = names(res_solo_models),
+         ~ mutate(.x, model = .y)) %>%
+    bind_rows() %>% arrange(.metric, desc(.estimate))
+  
+  p <- res_solo_models %>%
+    map2(names(.), ~ mutate(.x, model = .y)) %>%
+    map(group_by, model) %>%
+    map(roc_curve,
+        !!target_var, !!pred_positive) %>%
+    bind_rows() %>%
+    autoplot(size = ) +
+    labs(title = "ROC Curve")
+  
+  return(list(auc = tmp_auc, roc = p))
+}
+
+
+# threshold_f1score -------------------------------------------------------
+
+threshold_f1score <-
+  function(res_fit, lo=0.5, hi=0.95) {
+    res <- tibble()
+    for (thre in seq(lo, hi, 0.01)) {
+      tmp <-
+        res_fit %>%
+        select(target_var, contains(paste0("pred_", positive))) %>%
+        pivot_longer(-target_var,
+                     names_to = "model",
+                     values_to = "prob") %>%
+        group_by(model) %>%
+        mutate(truth = !!target_var,
+               pred = ifelse(prob >= thre, "WS", "PS")) %>%
+        mutate(pred = as.factor(pred)) %>%
+        f_meas(truth, pred) %>%
+        mutate(threshold = thre) %>%
+        rename(f1_score = .estimate)
+      res <- tmp %>% bind_rows(res)
+    }
+    
+    adjust_thre <-
+      res %>%
+      group_by(model) %>%
+      arrange(threshold) %>%
+      distinct() %>%
+      arrange(desc(f1_score)) %>%
+      distinct(model, .keep_all = TRUE)
+    
+    p <- res %>%
+      ggplot(aes(x = threshold, y = f1_score)) +
+      geom_point() +
+      labs(title = paste0("adjusted threshold for miximize f1_score is: ", adjust_thre$threshold)) +
+      geom_vline(xintercept = adjust_thre$threshold) +
+      geom_hline(yintercept = adjust_thre$f1_score) +
+      scale_x_continuous(n.breaks = 10) +
+      scale_y_continuous(n.breaks = 10)
+    
+    cat("\n res_f1, adjusted_threshold, and roc_plot are returned. \n")
+    
+    return(list(
+      res_f1 = res,
+      adjusted_threshold = adjust_thre,
+      thre_f1 = p
+    ))
+  }
 
 
 # threshold_f1score all ---------------------------------------------------------------
 
 threshold_f1score_all <-
-  function(baked_test, lo, hi) {
+  function(res_fit, lo=0.05, hi=0.95) {
     res <- tibble()
     for (thre in seq(lo, hi, 0.01)) {
       tmp <-
-        baked_test %>%
-        select(target_var, contains("pred_")) %>%
+        res_fit %>%
+        select(target_var, contains(paste0("pred_", positive))) %>%
         pivot_longer(-target_var,
                      names_to = "model",
                      values_to = "prob") %>%
@@ -216,8 +289,9 @@ threshold_f1score_all <-
     p <- res %>%
       ggplot(aes(x = threshold, y = f1_score, color = model)) +
       geom_point() +
-      labs(title = paste0("all models: threshold vs f1_score")) +
-      scale_x_continuous(n.breaks = 10)
+      labs(title = paste0("all mothreshold vs f1_score")) +
+      scale_x_continuous(n.breaks = 10) +
+      scale_y_continuous(n.breaks = 10)
     
     cat("\n res_f1, adjusted_threshold, and roc_plot are returned. \n")
     
@@ -227,3 +301,73 @@ threshold_f1score_all <-
       roc_plot = p
     ))
   }
+
+
+# predict by adjusted threshold -------------------------------------------
+
+predict_by_adjusted_threshold <-
+  function(res_models, adjusted_thre_models) {
+    res <-
+      map2(.x = res_models,
+           .y = adjusted_thre_models,
+           ~ mutate(
+             .x,
+             pred = ifelse(
+               !!pred_positive > .y$adjusted_threshold$threshold,
+               positive,
+               negative
+             ),
+             pred = as.factor(pred)
+           ))
+    cat("\n add pred column to the data by using adjusted threshold\n")
+    return(res)
+  }
+
+
+
+# confusion matrix all ----------------------------------------------------
+
+confusion_matrix_info <-
+  function(res_models) {
+    confusion_matrix_all <-
+      res_models %>%
+      map(.x = .,
+          ~ confusionMatrix(.x$pred, .x[[target_var]], positive = positive) %>% tidy)
+    
+    confusion_matrix_all <-
+      confusion_matrix_all %>%
+      bind_rows() %>%
+      mutate(model = rep(names(confusion_matrix_all),
+                         each = nrow(confusion_matrix_all[[1]]))) %>%
+      arrange(term, desc(estimate))
+    info <-
+      res_models %>%
+      map(.x = .,
+          ~ confusionMatrix(.x$pred, .x[[target_var]], positive = positive))
+    
+    cm <- res_models %>% map(conf_mat,
+                             truth = !!target_var,
+                             estimate = pred)
+    hm <- cm %>% map(autoplot, "heatmap") %>%
+      map2(names(cm),wrapper_add_title)
+    ms <- cm %>% map(autoplot, "mosaic") %>% 
+      map2(names(cm),wrapper_add_title)
+    
+    
+    return(
+      list(
+        info = info,
+        confusion_matrix_all = confusion_matrix_all,
+        heatmap = hm,
+        mosaic = ms,
+        cm = cm
+      )
+    )
+  }
+
+
+# wrapper add title gg ----------------------------------------------------
+
+wrapper_add_title <- function(p, model_names){
+  p + labs(title = model_names)
+}
